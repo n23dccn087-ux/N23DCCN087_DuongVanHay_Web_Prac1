b8928c258f58d0c43d15424e6509a907b5930d29
Duong Van Hay <n23dccn087@student.ptithcm.edu.vn>
2026-03-30 20:52:03 +0700

    feat: add footer component

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-8">
+      © 2025 MyStore - All rights reserved.
+    </footer>
+  );
+}
